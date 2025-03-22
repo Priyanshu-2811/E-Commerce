f2e0c333e4c8134c4ff47873f4ac69fad218b74b
Priyanshu Tiwari <140437871+Priyanshu-2811@users.noreply.github.com>
2025-03-22 04:52:57 +0000

    Initial

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     const user = true;
     const isAdmin = true;
-    return()
+    return(
         <header className='fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800'>
             <div className='container mx-auto px-4 py-3'>
                 <Link to='/' className='text-2xl font-bold text-emerald-400 items-center space-x-2 flex'>
@@ -42,7 +42,6 @@ const Navbar = () => {
                 </nav>
             </div>
     )   </header>
-    }
-};
+    )};
 
 export default Navbar;
